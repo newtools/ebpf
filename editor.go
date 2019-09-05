9647aa628c76a59f4119cbea3279aa416a3e5c04
Lorenz Bauer <lmb@cloudflare.com>
2019-09-05 17:16:13 +0100

    editor: simplify linking
    The current linking code verifies that no duplicate symbols exist
    in the resulting program, and tries to avoid work by linking all
    libraries at once. It returns an error if a function call can
    not be satisfied by any of the given libraries.
    
    The check for duplicate symbols is not necessary, since it's performed
    by asm.Instructions.Marshal later. It's also unclear how much work
    is really saved.

--- editor.go
+++ editor.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/newtools/ebpf/asm"
+
 	"github.com/pkg/errors"
 )
 
@@ -108,62 +109,57 @@ func (ed *Editor) RewriteConstant(symbol string, value uint64) error {
 //
 // Sections must not require linking themselves.
 func (ed *Editor) Link(sections ...asm.Instructions) error {
-	sections = append(sections, *ed.instructions)
+	var (
+		linked asm.Instructions
+		err    error
+	)
 
-	// A map of symbols to the libraries which contain them.
-	symbols := make(map[string]*asm.Instructions)
-	for i, section := range sections {
-		offsets, err := section.SymbolOffsets()
+	for _, section := range sections {
+		linked, err = link(*ed.instructions, linked, section)
 		if err != nil {
 			return err
 		}
-		for symbol := range offsets {
-			if symbols[symbol] != nil {
-				return errors.Errorf("symbol %s is present in multiple sections", symbol)
-			}
-			symbols[symbol] = &sections[i]
-		}
 	}
 
-	// Appending to ed.instructions would invalidate the pointers in
-	// ed, so instead we append to a new slice and join them at the end.
-	var linkedInsns asm.Instructions
-
-	// A list of already linked sections to avoid linking multiple times.
-	linkedSections := map[*asm.Instructions]struct{}{
-		ed.instructions: struct{}{},
+	if linked != nil {
+		// Append linked last to avoid traversing the linked sections
+		// in link().
+		*ed.instructions = append(*ed.instructions, linked...)
+		*ed = *Edit(ed.instructions)
 	}
-
-	for symbol, indices := range ed.ReferenceOffsets {
-		for _, index := range indices {
-			ins := &(*ed.instructions)[index]
-
-			if ins.OpCode.JumpOp() != asm.Call || ins.Src != asm.R1 {
-				continue
-			}
-
-			if ins.Constant != -1 {
-				// This is already a valid call, no need to link again.
-				continue
-			}
-
-			section := symbols[symbol]
-			if section == nil {
-				return errors.Errorf("symbol %s missing from libaries", symbol)
-			}
-
-			if _, ok := linkedSections[section]; !ok {
-				linkedInsns = append(linkedInsns, *section...)
-				linkedSections[section] = struct{}{}
-			}
-		}
-	}
-
-	// ed.instructions has been fixed up. Append linked instructions and
-	// recalculate ed.
-	*ed.instructions = append(*ed.instructions, linkedInsns...)
-	*ed = *Edit(ed.instructions)
 	return nil
+}
+
+func link(insns, linked, section asm.Instructions) (asm.Instructions, error) {
+	// A map of symbols to the libraries which contain them.
+	symbols, err := section.SymbolOffsets()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ins := range insns {
+		if ins.Reference == "" {
+			continue
+		}
+
+		if ins.OpCode.JumpOp() != asm.Call || ins.Src != asm.R1 {
+			continue
+		}
+
+		if ins.Constant != -1 {
+			// This is already a valid call, no need to link again.
+			continue
+		}
+
+		if _, ok := symbols[ins.Reference]; ok {
+			// At this point we know that at least one function in the
+			// section is called from insns, so append it to the linked instructions.
+			// The rewrite of ins.Constant happens in asm.Instruction.Marshal.
+			return append(linked, section...), nil
+		}
+	}
+
+	return linked, nil
 }
 
 type unreferencedSymbolError struct {
